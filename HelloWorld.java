8e9162b09a93dbb096d2acf9e4b5795491f567c1
HarideVos <53867807+HarideVos@users.noreply.github.com>
2019-08-07 15:59:45 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// Name: Hari de Vos
+// Assignment: Systel training AcadGild Assignment 1
+public class HelloWorld {
+	public static void main (String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
